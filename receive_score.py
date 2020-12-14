c335f84ddce24848a2ab9eb17472417cfad76fa1
63070171 <73688968+63070171@users.noreply.github.com>
2020-12-14 21:19:45 +0700

    Add files via upload

---
+++ receive_score.py
@@ -0,0 +1,16 @@
+"""receive and process"""
+def receive():
+    """gpax onet gat pat"""
+    gpax = float(input('GPAX : '))
+    onet = float(input('O-NET : '))
+    gat = float(input('GAT : '))
+    pat1 = float(input('PAT1 : '))
+    pat2 = float(input('PAT2 : '))
+    pat3 = float(input('PAT3 : '))
+    pat4 = float(input('PAT4 : '))
+    pat5 = float(input('PAT5 : '))
+    pat6 = float(input('PAT6 : '))
+    pat7 = float(input('PAT7 : '))
+    score_gpax = gpax*20/4
+    score_onet = onet*30/800
+receive()
